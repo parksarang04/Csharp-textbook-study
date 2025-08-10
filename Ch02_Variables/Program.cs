f1c5bfd50bcb9ff3bcf01b4c9d4da8d28bc8ec07
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-10 22:03:18 +0900

    초기 커밋.

---
+++ Ch02_Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ch02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, c# 교과서!");
+        }
+    }
+}
