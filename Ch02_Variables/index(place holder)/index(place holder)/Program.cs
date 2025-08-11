e55b974d325380f6db39eaf6ff5047fe43d7cba3
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-11 19:58:25 +0900

    자리표시자(인덱스) 표기

---
+++ Ch02_Variables/index(place holder)/index(place holder)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace index_place_holder_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", "Hello, C#");
+            Console.WriteLine("{0},{1}", "Hello", "C#");
+
+            Console.WriteLine("{1}", "{0}", "Hello", "C#"); //책 에서 결과물은 C#, Hello로 나오는데 실행결과는 Hello만 출력됨
+            //자리 표시자가 ""(큰 따움표) 밖을 넘어가면 평범한 문자열 값이 되서 책과 다른 결과물이 나옴
+
+            Console.WriteLine("{1} , {0}", "Hello", "C#"); //올바른 자리 표시자의 순서 변경
+        } 
+       
+
+    }
+}
---
+++ NewLine.cs/NewLine.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NewLine.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("줄 \n 바꿈");
+            Console.Write("줄 \n 바꿔지나?");
+
+            Console.WriteLine("안녕하\n 세요? ㅎ");
+            Console.Write("\n 처음부터 줄 바꿔~");
+        }
+        //\n 이러한 특수 기호를 이스케이프 시퀀스 라고 한다.
+    }
+}
