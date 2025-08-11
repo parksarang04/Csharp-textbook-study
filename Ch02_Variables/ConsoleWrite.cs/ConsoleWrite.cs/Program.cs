f3cbc8bd1a590fea2e71997c80265e8abd4d157f
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-11 18:43:07 +0900

    한 줄씩 출력할 떄는 Console.WriteLine() 줄 바꿈 없을 때는 Console.Write()

---
+++ Ch02_Variables/ConsoleWrite.cs/ConsoleWrite.cs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleWrite.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("줄 바꿈 없음");
+            Console.WriteLine("줄 바꿈 포함");
+        }
+    }
+}
