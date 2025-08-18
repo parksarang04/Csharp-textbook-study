230c0a333ebe8a0cbece812272da5cf3fed984ce
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-18 18:08:53 +0900

    const 상수는 한 번 상수로 지정된 변수는 다시 값을 바꿀 수 없고, 반드시 선언과 동시애 초기화 해줘야한다.

---
+++ Ch02_Variables/ConstantDemo.cs/ConstantDemo.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConstantDemo.cs
+{
+    internal class Program
+    {
+        //const 는 상수 또는 지역 상수 라고 한다.
+        static void Main(string[] args)
+        {
+            const int MAX = 100;    //선언과 동시에 초기화
+            Console.WriteLine("최댓값 : {0}", MAX);
+            //인덱스({0}) 값이 없으면 어디에 출력해야 할지 몰라서 적어줘야한다 => 규칙
+        }
+    }
+}
