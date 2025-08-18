aa45bae3ebf9efe5cf5bdfd14536e4bad5b79a02
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-18 18:03:26 +0900

    변수 지정하고 값 할당하기

---
+++ Ch02_Variables/Literal.cs/Literal.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Literal.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1234");      //정수 리터럴
+            Console.WriteLine(3.14F);       //실수 리터럴
+            Console.WriteLine('A');         //문자 리터럴
+            Console.WriteLine("HELLO");     //문자열 리터럴
+        }
+    }
+}
---
+++ Ch02_Variables/Variable.cs/Variable.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Variable.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 7;
+            Console.WriteLine(number);
+        }
+    }
+}
