0ae90360f32580bcb227a9dff1fdf4f7e1d3f0eb
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-18 18:15:56 +0900

    char 변수에 ''로 문자를 하나 이상 지정하면 문자 리터럴에 문자가 너무 많습니다 에러 표시.

---
+++ Ch02_Variables/CharacterDemo.cs/CharacterDemo.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CharacterDemo.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char grade = 'A';
+            char kor = '가';
+            //char ex = '가나';   => char 변수에 ''로 문자를 하나 이상 지정하면 문자 리터럴에 문자가 너무 많습니다 에러 표시.
+
+            Console.WriteLine(grade);
+            Console.WriteLine(kor);
+        }
+    }
+}
