3984cf6ce7d5c6c510b92668759ce1ca4c15f880
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-19 22:49:36 +0900

    Console.ReadLine() 메서드로 콘솔에서 값을 입력받아 변수에 저장한 후 사용

---
+++ Ch02_Variables/InputName.cs/InputName.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InputName.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("이름을 입력하시오 => ");
+            string name = Console.ReadLine();
+            Console.WriteLine("안녕하세요 {0}님", name);
+
+        }
+
+    }
+    
+
+}
