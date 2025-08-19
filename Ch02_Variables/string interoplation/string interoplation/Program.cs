494ab296fbc2f7de4414bb3431c744dcab78f412
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-19 22:39:09 +0900

    믄자열 보간법

---
+++ Ch02_Variables/string interoplation/string interoplation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace string_interoplation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            string result = "홀수";
+            Console.WriteLine($"{number} 은 {result} 입니다.");
+        }
+    }
+}
