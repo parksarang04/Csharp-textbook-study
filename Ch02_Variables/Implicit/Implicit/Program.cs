aa2918228effd45315d24beae806b7489a77e8e9
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-21 23:21:42 +0900

    암시적 형식 변환

---
+++ Ch02_Variables/Implicit/Implicit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace implicitExample
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int number1 = 1234;
+            long number2 = number1;    // number1 값을 그보다 큰 long 형식 변수인 number2에 저장
+            Console.WriteLine(number2);
+        }
+    }
+}
--- Ch02_Variables/TypeConversionErrors/TypeConversionErrors/Program.cs
+++ Ch02_Variables/TypeConversionErrors/TypeConversionErrors/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace TypeConversionErrors
+﻿using System;
+using System.Linq;
+
+namespace TypeConversionErrors;
+
+public static class Program
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        long l = long.MaxValue;
+        Console.WriteLine($"l의 값 : {l}");    //long 형식 변수의 가장 큰 값을 l변수에 저장
+        int i = (int)l;
+        Console.WriteLine($"i의 값 : {i}");    //l의 변수 값을 int 형식으로 변환하여 i 변수에 저장
     }
 }
