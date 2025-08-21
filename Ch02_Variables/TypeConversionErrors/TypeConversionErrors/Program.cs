83079020bb5851bfa50661db9030b211c5fece33
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-21 23:19:25 +0900

    묵시적 형식 변환의 오류

---
+++ Ch02_Variables/TypeConversionErrors/TypeConversionErrors/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypeConversionErrors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
