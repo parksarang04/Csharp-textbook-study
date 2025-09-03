c4a7eff897a0e7260ddf59c07b15eb0797f1a725
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 15:42:42 +0900

    study: 키보드 입력(ConsoleKeyInfo) 실습 코드 작성

---
+++ Ch02_Variables/KeyboardInput/KeyboardInput/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace KeyboardInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("아무거나 누르세요.");
+
+            ConsoleKeyInfo cki = Console.ReadKey(true);     //키보드 키 값 입력
+            Console.WriteLine("{0}", cki.Key);              //키
+            Console.WriteLine("{0}", cki.KeyChar);          //유니코드
+            Console.WriteLine("{0}", cki.Modifiers);        //Ctrl, Shift, Alt 조합
+            {
+                Console.WriteLine("Q를 입력하셨군요...");
+            }
+        }
+    }
+}
