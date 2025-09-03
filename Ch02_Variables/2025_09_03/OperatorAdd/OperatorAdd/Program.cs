bbade483496818648bc2a810b3a35ba00b6e4e2b
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 19:24:02 +0900

    study : 더하기 연산자 출력 실습 (int.Parse사용)

---
+++ Ch02_Variables/2025_09_03/OperatorAdd/OperatorAdd/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OperatorAdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 20;
+            int k = i + j;
+            Console.WriteLine(k);
+
+           var a = Console.ReadLine();
+           var b = Console.ReadLine();
+           var c = a + b;
+            Console.WriteLine(c);   // 숫자 연산이 아니고 문자열 붙여주기라서 이상하게 출력.
+
+            var d = int.Parse(Console.ReadLine());
+            var e = int.Parse(Console.ReadLine());
+            var f = d + e;
+            Console.WriteLine(f);   //문자열 -> 정수로 바꿔준다 (int.parse / Covert.ToInt32)
+        }
+    }
+}
--- Ch02_Variables/2025_09_03/default_2/default_2/Program.cs
+++ Ch02_Variables/2025_09_03/default_2/default_2/Program.cs
@@ -7,17 +7,8 @@ namespace default_2
     {
         static void Main(string[] args)
         {
-            int intDefault = default(int);        // int 형식의 기본값 (0)
-            Console.WriteLine(intDefault);
-
-            bool boolDefault = default(bool);     // bool 형식의 기본값 (false)
-            Console.WriteLine(boolDefault);
-
-            string stringDefault = default(string); // string 형식의 기본값 (null)
-            Console.WriteLine(stringDefault);
-
-            StringBuilder sbDefault = default(StringBuilder); // 참조 형식 기본값 (null)
-            Console.WriteLine(sbDefault);
+            double number =(long)3.141592;
+            Console.WriteLine(number);
         }
     }
 }
