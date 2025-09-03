9567e9fe8539d3e5c00dbc24efb67ca61f8d770b
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 15:32:04 +0900

    사용자가 입력하는  값을 var로 선언된 변수에 담아 사용할 수 있다.

---
+++ Ch02_Variables/VarInput/VarInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();                  //문자열 입력받기
+            var c = Convert.ToChar(Console.Read());     //문자 하나 입력받기
+            Console.WriteLine($"{s}:{s.GetType()}, {c} : {c.GetType()}");
+        }
+    }
+}
