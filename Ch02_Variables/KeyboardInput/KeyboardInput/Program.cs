8df0f559a35e30434b6e04122a125eb110929936
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 15:57:48 +0900

    study: default 키워드와 기본값 출력 예제 추가

--- Ch02_Variables/KeyboardInput/KeyboardInput/Program.cs
+++ Ch02_Variables/KeyboardInput/KeyboardInput/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("아무거나 누르세요.");
 
-            ConsoleKeyInfo cki = Console.ReadKey(true);     //키보드 키 값 입력
+            ConsoleKeyInfo cki = Console.ReadKey(false);     //키보드 키 값 입력
             Console.WriteLine("{0}", cki.Key);              //키
             Console.WriteLine("{0}", cki.KeyChar);          //유니코드
             Console.WriteLine("{0}", cki.Modifiers);        //Ctrl, Shift, Alt 조합
---
+++ Ch02_Variables/defauit/defauit/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace defauit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = default;
+            double d = default;
+            char c = default;
+            string s = default;
+            Console.WriteLine($"{i},{d},{c},{s}");                   //c = \c, s=null => 눈에 안보이는 결과값
+            Console.WriteLine($"[{i}],[{d}],[{c}],[{s}]");           //값이 비어도 대괄호가 양쪽에 붙어서 "여기에 출력된게 정말 없는 것인지, 아니면 안보이는 문자인지" 확인 할 수 있다.
+            Console.WriteLine($"[{i}],[{d}],[{c==Char.MinValue}],[{s==null}]");
+        }
+    }
+}
