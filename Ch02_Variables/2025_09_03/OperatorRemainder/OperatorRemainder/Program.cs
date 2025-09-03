6602ed092b68ed63f1a180ca226e5e9b4b58b9e8
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 19:38:46 +0900

    study : 나머지 연산자 출력 예제 (사용자의 입력을 받아 3의 배수 출력)

---
+++ Ch02_Variables/2025_09_03/OperatorRemainder/OperatorRemainder/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OperatorRemainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            int j = 3;
+            int k = i % j;
+            Console.WriteLine(k);
+            Ex();
+        }
+
+        public static void Ex()
+        {
+            var a = int.Parse(Console.ReadLine());
+
+            if (a % 3 ==0)
+            {
+                Console.WriteLine($"{a}는 3의 배수 입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{a}는 3의 배수가 아닙니다");
+            }
+        }
+    }
+}
