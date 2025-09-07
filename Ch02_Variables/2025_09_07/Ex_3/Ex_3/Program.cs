fd43c25636c177a076813f987b02ac1c86293f5d
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 23:41:44 +0900

    study : 비트 AND 연산자(&)를 사용한 예제

---
+++ Ch02_Variables/2025_09_07/Ex_3/Ex_3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 0b1010;
+            byte y = 0b1100;
+            Console.WriteLine($" {Convert.ToString(x, 2)} -> {x}");
+            Console.WriteLine($"&{Convert.ToString(y, 2)} -> {y}");
+
+            Console.WriteLine($" {Convert.ToString(x & y, 2)}-> {x & y,2}");
+        }
+    }
+}
