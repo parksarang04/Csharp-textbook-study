31240ea5a350e715e823195aa5137e3522ce1277
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 22:14:05 +0900

    syudy : 관계형 연산자 예제

---
+++ Ch02_Variables/2025_09_07/Ex_2/Ex_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = x++;
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+
+            Operator();
+        }
+
+        static void Operator()
+        {
+            int x = 3;
+            int y = 5;
+            Console.WriteLine(x == y);  //false
+            Console.WriteLine(x != y);  //true
+            Console.WriteLine(x >= y);  //false
+            Console.WriteLine(x <= y);  //true
+        }
+    }
+}
