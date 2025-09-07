f846f8af60ad7c21cc538fb1c9d0cd0c12afa685
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 10:15:42 +0900

    study: ToString()과 Convert.ToString()메서드를 이용한 숫자 형식을 문자열 형식으로 변경하는 예제

---
+++ Ch02_Variables/2025_09_07/Ex_1/Ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = 28;
+            Console.WriteLine($"2월달은 {days} 일 입니다.");
+            Console.WriteLine("2월 달은 " + days + "일입니다.");
+            Console.WriteLine("2월 달은" + days.ToString() + " 일 입니다.");
+            Console.WriteLine("2월 달은" + Convert.ToString(days) + " 일 입니다.");
+        }
+    }
+}
