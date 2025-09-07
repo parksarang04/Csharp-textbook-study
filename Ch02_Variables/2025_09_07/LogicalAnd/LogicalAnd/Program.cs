790b30c213e56378c349219b4e3df2ba17b3abcc
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 22:18:21 +0900

    study : &&연산자의 예제

---
+++ Ch02_Variables/2025_09_07/LogicalAnd/LogicalAnd/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicalAnd
+{
+    internal class Program
+    {
+        // && 연산자는 둘 다 참일 때만 참을 반환
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"true && true -> {true && true}");
+            Console.WriteLine($"true && false -> {true && false}");
+            Console.WriteLine($"false && true -> {false && true}");
+            Console.WriteLine($"false && false -> {false && false}");
+        }
+    }
+}
