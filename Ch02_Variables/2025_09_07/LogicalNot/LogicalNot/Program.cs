e81732176ff7eb410d7fef9bbdcea80532da331a
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 22:25:43 +0900

    study : ! 연산자 예제풀이

---
+++ Ch02_Variables/2025_09_07/LogicalNot/LogicalNot/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LogicalNot
+{
+    internal class Program
+    {
+        //! 연산자는 조건식 결과를 반대로 바꾸어준다.
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"!true ->{!true}");
+            Console.WriteLine($"!false -> {!false}");
+        }
+    }
+}
