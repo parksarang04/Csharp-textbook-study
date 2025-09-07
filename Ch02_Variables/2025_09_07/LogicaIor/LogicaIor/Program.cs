8ccadc60ad5cac0425625ba968106476988ca68b
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 22:22:17 +0900

    stduy : || 연산자 예제

---
+++ Ch02_Variables/2025_09_07/LogicaIor/LogicaIor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicaIor
+{
+    internal class Program
+    {
+        // ||연산자는 하나라도 참이면 참을 반환한다. 둘 다 거짓일 때는 거짓을 반환한다.
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"true || true -> {true || true}");
+            Console.WriteLine($"true || fales -> {true || false}");
+            Console.WriteLine($"fales || true -> {false || true}");
+            Console.WriteLine($"fales || fales -> {false | false}");
+        }
+    }
+}
