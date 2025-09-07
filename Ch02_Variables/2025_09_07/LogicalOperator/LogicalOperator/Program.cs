3dab5ae866d25626e9847f323092e3fac271d4da
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-07 22:41:04 +0900

    study : &&, ||, ! 논리 연산자를 사용한 예제

---
+++ Ch02_Variables/2025_09_07/LogicalOperator/LogicalOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 3;
+            var j = 5;
+            var r = false;
+            
+            if ((i == 3) && (j !=5) || (i !=3) || (j==3))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                {
+                    Console.WriteLine(false);
+                }
+            }
+
+            r = (i >= 5);
+            Console.WriteLine("{0}", !r);
+        }
+    }
+}
