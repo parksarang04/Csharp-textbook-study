827822fd2fc6ffafe056a691bf2756d2d63c43e2
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-09 19:58:24 +0900

    study: 순차문 순서대로 실행하기 예습(평균값 구하기)

---
+++ Ch02_Variables/2025_09_09/SequenceDemo/SequenceDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace SequenceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kor = 100;
+            int eng = 90;
+
+            int total = 0;
+            double avg = 0.0;
+
+            total = kor + eng;
+            avg = total / 2.0;
+
+            Console.WriteLine($"총점 : {total}");
+            Console.WriteLine($"평균 : {avg:F1}");
+        }
+    }
+}
