3f84bd9f36168e66d939f8d7bd6d7f803a82406b
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-09 20:59:13 +0900

    study : 중첩 if문 예습

---
+++ Ch02_Variables/2025_09_09/IfNested/IfNested/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IfNested
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "C#";
+            int version = 8;
+
+            if (name == "C#")
+            {
+                if (version == 8)
+                {
+                    Console.WriteLine($"{name} {version}");
+                }
+            }
+            Main2(args);
+        }
+
+        static void Main2(string[] args)
+        {
+            int number = 1234;
+            if (number == 1234 && number >= 1000)
+            {
+                Console.WriteLine("맞습니다");
+            }
+            if (number ==1234 || number <= 1000)
+            {
+                Console.WriteLine("하나라도 참이면 참");
+            }
+        }
+    }
+}
