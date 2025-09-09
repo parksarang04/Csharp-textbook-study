5716fd8eba2a51b28dff5bdffb12bc7ff203a4d2
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-09 20:44:32 +0900

    study : if문 단문 예습

---
+++ Ch02_Variables/2025_09_09/If/If/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int scroe = 60;
+
+            if (scroe >= 60)
+            {
+                Console.WriteLine("합격");
+            }
+        }
+    }
+}
