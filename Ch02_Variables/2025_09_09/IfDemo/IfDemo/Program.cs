b2af3d42c707e3155717370dd1cb0b4da4122c94
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-09 20:50:05 +0900

    study : if문 복문 예습

---
+++ Ch02_Variables/2025_09_09/IfDemo/IfDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IfDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            if(x == 10)
+            {
+                Console.WriteLine($"x는{x} 입니다");
+            }
+            if (x !=20)
+            {
+                Console.WriteLine($"x는 20이 아닙니다.");
+            }
+
+            Test();
+        }
+
+        static void Test()
+        {
+            string s1 = "Hello";
+            string s2 = "Hello";
+
+            if (s1 ==s2)
+            {
+                Console.WriteLine("Same");
+            }
+        }
+    }
+}
