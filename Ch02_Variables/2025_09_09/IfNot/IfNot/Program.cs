9c0afe1b0629823238e81e7f28b393eca89425cb
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-09 20:53:27 +0900

    study : !(NOT) 연사자를 if 문의 조건식 사용 예제

---
+++ Ch02_Variables/2025_09_09/IfNot/IfNot/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IfNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bln = false;
+            if (!bln)
+            {
+                Console.WriteLine("bln : false -> ! -> true");
+            }
+        }
+    }
+}
