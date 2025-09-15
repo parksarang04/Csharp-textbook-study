44292c70fd9066ce9ad7ac2aa66b774faf2a5195
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 15:39:40 +0900

    입력받은 점수에 따라 상장 수여 예제 (중첩 if~ if else)

---
+++ Ch02_Variables/2025_09_15/IfElseScroeGrade/IfElseScroeGrade/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IfElseScroeGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("점수 : ");
+            int scroe = Convert.ToInt32(Console.ReadLine());
+            string geade;
+
+            if (scroe >= 90)
+            {
+                geade = "금메달";
+            }
+            else
+            {
+                if (scroe >= 80)
+                {
+                    geade = "은메달";
+                }
+                else
+                {
+                    if (scroe >= 70)
+                    {
+                        geade = "동메달";
+                    }
+                    else
+                    {
+                        geade = "메달없음";
+                    }
+                }
+            }
+            Console.WriteLine($"{geade} 을 수상했습니다.");
+        }
+    }
+}
