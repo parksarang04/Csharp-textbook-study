a437961dea462f64282df09fd1c7d02b2f9cee52
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 15:43:36 +0900

    study : 양수, 음수, 0을 판단하는 다중 if 문 예제

---
+++ Ch02_Variables/2025_09_15/PositiveNegativeZero/PositiveNegativeZero/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PositiveNegativeZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = -10;
+
+            if (number > 0)
+            {
+                Console.WriteLine($"{number} 은 양수입니다.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"{number} 은 음수입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} 은 0 입니다.");
+            }
+               
+        }
+    }
+}
