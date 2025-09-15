e24eede272ac153b58a06ceefb66ec373d4e9a25
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 15:32:50 +0900

    study : 입력받은 문자에 따라 Yes, No, Cancle 출력 예제

---
+++ Ch02_Variables/2025_09_15/Yes, No, Cancel/Yes, No, Cancel/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Yes__No__Cancel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("문자를 입력하세여 (y,n ) : ");
+            char input = Convert.ToChar(Console.ReadLine());
+            if (input == 'y')
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                if (input == 'n')
+                {
+                    Console.WriteLine("No");
+                }
+                else
+                {
+                    {
+                        Console.WriteLine("Cancle");
+                    }
+                }
+            }
+            
+        }
+    }
+}
