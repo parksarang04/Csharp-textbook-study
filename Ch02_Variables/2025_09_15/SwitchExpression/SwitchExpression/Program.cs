7ad573da5e9e20768b8c840dc4ced760bc255fa4
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 16:16:25 +0900

    study : switch 문 기본 예제 및 입력 값에 따른 출력 예제

---
+++ Ch02_Variables/2025_09_15/SwitchExpression/SwitchExpression/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SwitchExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("1입니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("2입니다.");
+                    break;
+
+                
+            }
+            Ex_1();
+        }
+
+        static void Ex_1()
+        {
+            Console.WriteLine("정수를 입력하세요");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            switch(answer)
+            {
+                case 1:
+                    Console.WriteLine("1을 선택했습니다.");
+                    break;
+                case 2:
+                    Console.WriteLine("2를 선택했습니다.");
+                    break;
+                case 3:
+                    Console.WriteLine("3를 선택했습니다.");
+                    break;
+                default:
+                    Console.WriteLine("그냥 찍으셨군요");
+                    break;
+            }
+                
+        }
+
+
+    }
+}
