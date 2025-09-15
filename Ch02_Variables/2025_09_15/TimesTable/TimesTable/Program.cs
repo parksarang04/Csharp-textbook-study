e1c94646b763b629e7e52a1af35da3b2db2bd353
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 22:49:37 +0900

    study : 구구단 가로로 출력예제

---
+++ Ch02_Variables/2025_09_15/TimesTable/TimesTable/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TimesTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =2; i <= 9; i++)
+            {
+                Console.Write($"{i,4}단 ");
+            }
+            Console.WriteLine();
+
+            for (int i =1; i <= 9; i++)
+            {
+                for (int j = 2; j <=9; j++)
+                {
+                    Console.Write($"{j}*{i} = {j * i,2}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
