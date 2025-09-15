7c5eae297e1230e379c75b5d1c6952481cafbd19
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 14:11:41 +0900

    study : else문으로 두 가지 조건 처리하기 예제

---
+++ Ch02_Variables/2025_09_15/Else/Else/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 59;
+
+            if (score >= 60)
+            {
+                Console.WriteLine("합격");
+            }
+            else
+            {
+                Console.WriteLine("불합격");
+            }
+        }
+    }
+}
