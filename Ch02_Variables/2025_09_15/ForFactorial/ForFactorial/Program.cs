cac329464c859f1431a450b9e1c756ca78fe4f24
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 22:44:43 +0900

    study : 1부터 4까지 팩토리얼 값 출력

---
+++ Ch02_Variables/2025_09_15/ForFactorial/ForFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 0;
+
+            for (int i =1; i<=4; i++)
+            {
+                Console.Write($"{i}! -> ");
+                fact = 1;
+                for (int j =1; j <= i; j++)
+                {
+                    fact = fact * j;
+                }
+                Console.WriteLine(fact);
+            }
+        }
+    }
+}
