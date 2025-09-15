0f8209342c010372d577c111fa6ac2eca8660b34
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 14:09:14 +0900

    study : out 변수와 if 문으로 형식 변환하기

---
+++ Ch02_Variables/2025_09_15/OutVariable/OutVariable/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace OutVariable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            if (int.TryParse("안녕",out r))
+            {
+                // "안녕"은 int 형으로 변환이 불가능 하기에 이 코드는 실행되지 않습니다.
+                Console.WriteLine("{0}", r);
+            }
+            if (int.TryParse("1234",out var result))
+            {
+                // "1234" 는 itn 형식으로 반환이 가능하기에 result 선언과 동시에 1234가 저장됨
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
