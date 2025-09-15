f5c11947eba935336594c55ae444784438db7e5b
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 16:25:20 +0900

    study :  switch 문을 활용한 입력처리 예제 및 case 레이블에 문자열로 값을 비교하는 예제

---
+++ Ch02_Variables/2025_09_15/SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("가장 좋아하는 프로그래밍 언어는?");
+            Console.Write("1. C\t");
+            Console.Write("2. C++\t");
+            Console.Write("3. C#\t");
+            Console.Write("4. Java\t");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("C 선택");
+                    break;
+                case 2:
+                    Console.WriteLine("C++ 선택");
+                    break;
+                case 3:
+                    Console.WriteLine("C# 선택");
+                    break;
+                default:
+                    Console.WriteLine("Java 선택");
+                    break;
+
+            }
+            Ex();
+        }
+        static void Ex()
+        {
+            Console.WriteLine("오늘 날씨는 어떤가요? (맑음, 흐림, 비, 눈 ...)");
+            string weather = Console.ReadLine();
+
+            switch (weather)
+            {
+                case "맑음":
+                    Console.WriteLine("오늘 날씨는 맑군요.");
+                    break;
+                case "흐림":
+                    Console.WriteLine("오늘 날씨는 흐리군요.");
+                    break;
+                case "비":
+                    Console.WriteLine("오늘 날씨는 비가 오는군요.");
+                    break;
+                default:
+                    Console.WriteLine("혹시 눈이 내리나요?.");
+                    break;
+            }
+        }
+    }
+}
