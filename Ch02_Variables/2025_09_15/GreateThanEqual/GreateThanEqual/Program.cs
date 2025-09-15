48a125622194ab65a28525f3381b4f11a7eabf29
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 14:21:49 +0900

    study : 두 수를 입력한 후 그중 큰 수를 출력하는 예제

---
+++ Ch02_Variables/2025_09_15/GreateThanEqual/GreateThanEqual/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GreateThanEqual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("첫 번째 수 : ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("두 번째 수 : ");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            if (first >= second)
+            {
+                Console.WriteLine("큰 값 {0}" , first);
+            }
+            else
+            {
+                Console.WriteLine("큰 값 {0}", second);
+            }
+        }
+    }
+}
