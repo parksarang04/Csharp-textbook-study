0d5482d5481e6c5789aeb515157c33350fe50129
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-15 22:06:44 +0900

    study: 별 찍기 모래시계 예제

---
+++ Ch02_Variables/2025_09_15/ForTriangle(별찍기)/ForTriangle(별찍기)/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForTriangle_별찍기_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i >=1; i--)
+            {
+                for (int j= 1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i =2; i <=5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
