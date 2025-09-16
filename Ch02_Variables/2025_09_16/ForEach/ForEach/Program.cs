dc4c0ed9bb12233870b21eb0fa911ff032f06ff2
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-16 17:38:49 +0900

    study : foreach 문 예제

---
+++ Ch02_Variables/2025_09_16/ForEach/ForEach/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 문자열에서 문자 하나씩 뽑아 출력
+            string str = "ABC123";
+
+            foreach (char c in str)
+            {
+                Console.WriteLine($"{c}\t");
+            }
+            Console.WriteLine();
+
+            Ex_1();
+        }
+
+        static void Ex_1()
+        {
+            // 문자열에서 문자 하나씩 뽑아 출력2
+
+            var str = "ABC123";
+
+            foreach (var c in str)
+            {
+                Console.WriteLine($"{c}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
