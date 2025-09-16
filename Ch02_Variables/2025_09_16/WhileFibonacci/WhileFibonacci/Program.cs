fc6145d20e086f387ef505f96fceba60141d717a
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-16 17:17:17 +0900

    study : while 문으로 피보나치 수열 표현 예제

---
+++ Ch02_Variables/2025_09_16/WhileFibonacci/WhileFibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WhileFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 피보나치 수열 : 1 1 2 3 5 8 1 3 21
+
+            int first = 0;
+            int second = 1; 
+
+            while(second <= 20)
+            {
+                Console.WriteLine(second);
+                int temp = first + second;
+                first = second;
+                second = temp;
+            }
+        }
+
+    }
+}
