52dc0fb201b8906ef6208e6c504ac5b98820e3de
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-17 14:32:08 +0900

    study : 기본 매개변수 예제

---
+++ Ch02_Variables/함수/XML 문서 주석 사용_FunctionAddNumbers/XML 문서 주석 사용_FunctionAddNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace XML_문서_주석_사용_FunctionAddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///<summary>
+            ///두 수를 더하여 그 결과값을 반환시켜 주는 함수
+            ///</summary>
+            ///<param name="a"> 첫 번째 매개변수</param>
+            ///<param name="b"> 두 번째 매개변수</param>
+            ///<returns>a+b의 결과값</returns>
+            static int AddNumbers(int a, int b)
+            {
+                return a + b;
+            }
+
+            int a = 3;
+            int b = 5;
+            int c = AddNumbers(3, 5);
+            Console.WriteLine($"{a}+{b} = {c}");
+        }
+    }
+}
---
+++ Ch02_Variables/함수/기본 매개변수 사용_DefaultParamenter/기본 매개변수 사용_DefaultParamenter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace 기본_매개변수_사용_DefaultParamenter
+{
+    internal class Program
+    {
+        // 기본 매개변수 사용
+        static void Main(string[] args)
+        {
+            Log("디버그");
+            Log("에러",4);
+        }
+
+        static void Log(string message, byte level =1)
+        {
+            Console.WriteLine($"{message},{level}");
+        }
+    }
+}
