376c078ed6d2f2ec9dbf01ab302919f5fbd04040
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 18:13:12 +0900

    study : 열거형으로 관련있는 항목 묶기 예제

---
+++ Ch02_Variables/열거형/열거형으로 관련 있는 항목 묶기/열거형으로 관련 있는 항목 묶기/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace 열거형으로_관련_있는_항목_묶기
+{
+    class EnumAnimal
+    {
+        enum Animal { Mouse, Cow, Tiger }
+
+        static void Main()
+        {
+            Animal animal = Animal.Tiger;
+            Console.WriteLine(animal);
+
+            if (animal == Animal.Tiger)
+            {
+                Console.WriteLine("호랑이");
+            }
+        }
+    }
+}
