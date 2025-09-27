2e192c3aec97f608758bf0e6cea5462eccac0675
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 14:33:35 +0900

    study : 닷넷 API 예제. (Math 클래스와 using static 구문 함께 사용)

---
+++ Ch02_Variables/닷넷API/Math 클래스와 using atatic 구문 함께 사용/Math 클래스와 using atatic 구문 함께 사용/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Math;
+using static System.Console;
+
+namespace Math_클래스와_using_atatic_구문_함께_사용
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //기본 사용 방식
+            WriteLine(Math.Pow(2, 10));
+
+            // using static 지시문으로 줄여서 표현
+            WriteLine(Pow(2, 10));
+            WriteLine(Max(3, 5));
+        }
+    }
+}
