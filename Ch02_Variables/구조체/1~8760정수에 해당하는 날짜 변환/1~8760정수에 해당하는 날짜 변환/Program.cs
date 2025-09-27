dc509adb8e401445043da32592703d76889996f5
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 17:39:22 +0900

    study: DateTime 구조체의 AddHours() 메서드 사용하여 날자를 반환하는 함수 예제

---
+++ Ch02_Variables/구조체/1~8760정수에 해당하는 날짜 변환/1~8760정수에 해당하는 날짜 변환/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1_8760정수에_해당하는_날짜_변환
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetDateTimeFromYearlyHourNumber(1));
+            Console.WriteLine(GetDateTimeFromYearlyHourNumber(8760/2));
+            Console.WriteLine(GetDateTimeFromYearlyHourNumber(8760));
+        }
+
+        static DateTime GetDateTimeFromYearlyHourNumber(int number)
+        {
+            return (new DateTime(2025, 1, 1, 0, 0, 0)).AddHours(--number);
+        }
+    }
+}
