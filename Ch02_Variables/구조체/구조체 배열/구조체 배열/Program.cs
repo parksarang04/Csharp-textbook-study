e2f31443cfe0a1e6870f70752ba9a87b987b492f
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 15:30:05 +0900

    study : 구조체 배열을 사용하여 데이터 대입 및 출력 에제

---
+++ Ch02_Variables/구조체/구조체 배열/구조체 배열/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace 구조체_배열
+{
+    //BusinessCard , Name, Age
+
+    struct BusinessCard
+    {
+        public string Name;
+        public int Age;
+    }
+
+    class StructArry
+    {
+        static void Print(string name, int age)
+            => Console.WriteLine($"{name}은 {age}입니다.");
+
+        static void Main()
+        {
+            BusinessCard biz;
+            biz.Name = "박사랑";
+            biz.Age = 25;
+            Print(biz.Name, biz.Age);
+
+            BusinessCard[] names = new BusinessCard[2];
+            names[0].Name = "장경민"; names[0].Age = 24;
+            names[1].Name = "이연주"; names[1].Age = 26;
+            for(int i = 0; i < names.Length; i++)
+            {
+                Print(names[i].Name, names[i].Age);
+            }
+        }
+    }
+}
