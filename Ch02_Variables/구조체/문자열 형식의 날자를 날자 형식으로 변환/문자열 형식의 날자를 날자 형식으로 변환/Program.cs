fec42ccd071b9a651b3f473b60eeed379538cda5
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 17:34:15 +0900

    study: 인라인 out 변수로 문자열 형식의 날자를 날자 형식으로 변환

---
+++ Ch02_Variables/구조체/문자열 형식의 날자를 날자 형식으로 변환/문자열 형식의 날자를 날자 형식으로 변환/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace 문자열_형식의_날자를_날자_형식으로_변환
+{
+    class OutVariableDemo
+    {
+        static void Main()
+        {
+            if (DateTime.TryParse("2025/12/25", out DateTime/*var*/ xmas))
+            {
+                Console.WriteLine(xmas);
+            }
+        }
+    }
+}
