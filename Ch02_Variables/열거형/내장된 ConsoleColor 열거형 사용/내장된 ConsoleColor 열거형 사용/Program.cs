65b6cc46e474ed0a90e360fc13e64a6557ad855a
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 18:04:05 +0900

    study: 내장된 ConsoleColor 열거형 사용 예제

---
+++ Ch02_Variables/열거형/내장된 ConsoleColor 열거형 사용/내장된 ConsoleColor 열거형 사용/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 내장된_ConsoleColor_열거형_사용
+{
+    class ConsoleColorDemo
+    {
+        static void Main()
+        {
+            //전경색
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Blue");
+            Console.ResetColor();
+
+            //배경색
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Red");
+            Console.ResetColor();
+        }
+    }
+}
