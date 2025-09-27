7158cd1b22b71234beedf6a123b127ecbbcc0129
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 18:20:23 +0900

    study : 열거형 값을 정수형이나 문자열로 사용

---
+++ Ch02_Variables/열거형/열거형 값을 정수형이나 문자열로 사용/열거형 값을 정수형이나 문자열로 사용/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace 열거형_값을_정수형이나_문자열로_사용
+{
+    enum Animal { Rabbit, Dragon, Snake }
+
+    class EnumIndex
+    {
+        static void Main()
+        {
+            Animal animal = Animal.Dragon;
+            int num = (int)animal;
+            string str = animal.ToString();
+            Console.WriteLine($"Animal.Dragon : {num},{str}");
+        }
+    }
+}
