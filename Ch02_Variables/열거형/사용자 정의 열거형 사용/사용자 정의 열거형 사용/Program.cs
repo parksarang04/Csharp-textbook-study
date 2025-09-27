90be5864622c7cbda9a7f2e73aea00c4302391ed
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 18:09:21 +0900

    study : 사용자 정의 열거형 기본 예제

---
+++ Ch02_Variables/열거형/사용자 정의 열거형 사용/사용자 정의 열거형 사용/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace 사용자_정의_열거형_사용
+{
+    enum Priority   //우선순위를 묶어 관리할 수 있는 Priority 열거형 사용
+    {
+        High,
+        Normal,
+        Low
+    }
+
+    class EnumrationDemo
+    {
+        static void Main()
+        {
+            Priority high = Priority.High;  //인텔리센스의 도움을 받을 수 있어 편리함
+            Priority noraml = Priority.Normal;
+            Priority low = Priority.Low;
+
+            Console.WriteLine($"{high}, {noraml}, {low}");
+        }
+    }
+}
