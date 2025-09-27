1714e3009b12880f31fb04a7d693b64d236d250e
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-27 14:54:49 +0900

    study : 구조체를 사용하여 변수 하나로 여러 개를 묶어 관리하기 예제

---
+++ Ch02_Variables/구조체/구조체를 사용하여 변수 하나로 여러개 묶어 관리/구조체를 사용하여 변수 하나로 여러개 묶어 관리/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace 구조체를_사용하여_변수_하나로_여러개_묶어_관리
+{
+    class StructVariable
+    {
+        static void Main()
+        {
+            Product product;    //구조체 형식 변수 선언
+
+            //구조체 변수의 각 멤버의 값을 할당
+            product.Id = 1;
+            product.Title = "좋은 책";
+            product.Price = 100000M;
+
+            //구조체 형식 변수 선언
+            string message = $"번호 : {product.Id}\n상품명 :{product.Title}\n 가격 : {product.Price}";
+            Console.WriteLine(message);
+        }
+    }
+    struct  Product
+    {
+        public int Id;
+        public string Title;
+        public decimal Price;
+    }
+}
---
+++ Ch02_Variables/구조체/구조체를 사용하여 변수나 배열을 묶어 관리/구조체를 사용하여 변수나 배열을 묶어 관리/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 구조체를_사용하여_변수나_배열을_묶어_관리
+{
+    struct Point
+    {
+        public int X;
+        public int Y;
+    }
+
+    class StructDemo
+    {
+        static void Main()
+        {
+            Point point;     //Point 구조체 변수 point 선언
+            point.X = 100;
+            point.Y = 200;
+            Console.WriteLine($"X : {point.X}, Y : {point.Y}");
+        }
+    }
+
+   
+}
